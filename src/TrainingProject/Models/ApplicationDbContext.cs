8d757bdd75283c558cae3f549706684d793f8b8c
DarienTLOD <https://github.com/DarienTLOD>
2016-05-01 23:56:08 +0000

    Created model and context for user registration

---
+++ src/TrainingProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Data.Entity;
+
+namespace TrainingProject.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
