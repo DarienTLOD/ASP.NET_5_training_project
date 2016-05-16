0c3589fae79b31dd2fd5ed6a41ee5905e714b6da
DarienTLOD <darienyry@gmail.com>
2016-05-16 05:22:54 +0000

    Added files for doing lab 2.

--- src/TrainingProject/Controllers/HomeController.cs
+++ src/TrainingProject/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
 
 namespace TrainingProject.Controllers
@@ -14,6 +11,12 @@ namespace TrainingProject.Controllers
         }
 
         public IActionResult Error()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Task()
         {
             return View();
         }
