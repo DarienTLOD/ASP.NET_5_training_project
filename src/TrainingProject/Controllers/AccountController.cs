80d2fe065e891766d30eadab49dab07a3040bcfe
DarienTLOD <darienyry@gmail.com>
2016-05-16 01:37:03 +0000

    Added AccountController and method for user authentication based on cookies.

---
+++ src/TrainingProject/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using TrainingProject.Models;
+
+namespace TrainingProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var user = await _db.ApplicationUsers.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+            if (user != null)
+            {
+                await Authenticate(model.Email);
+
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            return View(model);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+                    {
+                        new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+                    };
+            var id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.Authentication.SignInAsync("Cookies", new ClaimsPrincipal(id));
+        }
+
+    }
+}
